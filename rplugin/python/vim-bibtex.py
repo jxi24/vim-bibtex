a318852702e0b64db8d571d2a649ea52d6259e31
Joshua Isaacson <isaacson@fnal.gov>
2019-11-01 14:22:41 -0500

    Initialize repository

---
+++ rplugin/python/vim-bibtex.py
@@ -0,0 +1,15 @@
+"""
+Neovim plugin to find all citations in a tex document
+and generate either a bibtex or latex file for the bibliography.
+"""
+import neovim
+
+
+@neovim.plugin
+class Bibtex:
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.function('vim-bibtex')
+    def vim_bibtex(self, args):
+        self.vim.command('echo "Test"')
