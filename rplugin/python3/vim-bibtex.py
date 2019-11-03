2ca72de325f14e2c3f0c26c559ff53427209893f
Joshua Isaacson <isaacson@fnal.gov>
2019-11-02 21:30:40 -0500

    Implement basic inspire implementation

--- rplugin/python3/vim-bibtex.py
+++ rplugin/python3/vim-bibtex.py
@@ -3,18 +3,18 @@ Neovim plugin to find all citations in a tex document
 and generate either a bibtex or latex file for the bibliography.
 """
 import pynvim
-import requests
+import inspire
 
 
 @pynvim.plugin
 class Limit(object):
     def __init__(self, vim):
         self.vim = vim
+        self.inspire = inspire.Inspire()
 
     @pynvim.function('Bibtex')
     def vim_bibtex(self, args):
-        result = requests.get('http://inspirehep.net/search?p='
-                              'f+a+joshua+isaacson&em=B&of=hx')
-        text = result.text.replace('</pre>', ' ').split('\n')
+        result = self.inspire.search('f a joshua isaacson')
+        text = result.replace('</pre>', ' ').split('\n')
         text = [x for x in text if '<pre>' not in x]
         self.vim.current.buffer.append(text)
