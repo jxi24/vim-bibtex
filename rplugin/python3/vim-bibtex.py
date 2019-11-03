80a0da4ef2f3b4fd7f86e32a4b479d6b711b023d
Joshua Isaacson <isaacson@fnal.gov>
2019-11-02 19:54:43 -0500

    Implment basic request

--- rplugin/python3/vim-bibtex.py
+++ rplugin/python3/vim-bibtex.py
@@ -3,39 +3,15 @@ Neovim plugin to find all citations in a tex document
 and generate either a bibtex or latex file for the bibliography.
 """
 import pynvim
+import requests
+
 
 @pynvim.plugin
 class Limit(object):
     def __init__(self, vim):
         self.vim = vim
-        self.calls = 0
 
-    @pynvim.command('Cmd', range='', nargs='*', sync=True)
-    def command_handler(self, args, range):
-        self._increment_calls()
-        self.vim.current.line = (
-            'Command: Called %d times, args: %s, range: %s' % (self.calls,
-                                                               args,
-                                                               range))
-
-    @pynvim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")',
-                    sync=True)
-    def autocmd_handler(self, filename):
-        self._increment_calls()
-        self.vim.current.line = (
-            'Autocmd: Called %s times, file: %s' % (self.calls, filename))
-
-    @pynvim.function('Func')
-    def function_handler(self, args):
-        self._increment_calls()
-        self.vim.current.line = (
-            'Function: Called %d times, args: %s' % (self.calls, args))
-
-    def _increment_calls(self):
-        if self.calls == 5:
-            raise Exception('Too many calls!')
-        self.calls += 1
-
-    @pynvim.function('TestCommand')
+    @pynvim.function('Bibtex')
     def vim_bibtex(self, args):
-        self.vim.current.line = 'New line'
+        result = requests.get('http://inspirehep.net/search?p=f+a+joshua+isaacson&em=B&of=hx')
+        self.vim.current.line = result.text
